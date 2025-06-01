73a8d5f0556851cdd121d672577c68af86fdbf51
diosvanytorres3@gmail.com <130931255+otura41@users.noreply.github.com>
2025-06-01 16:48:28 -0400

    regresando

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from functools import lru_cache
-from typing import Union
-
 from fastapi import FastAPI, Depends
 from fastapi.responses import PlainTextResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
@@ -17,10 +14,10 @@ app = FastAPI()
 app.include_router(todos.router)
 
 
-#origins = [
-#    "http://localhost:3000",
-#    "https://todo-frontend-khaki.vercel.app/",
-#]
+origins = [
+    "http://localhost:3000",
+    "https://todo-frontend-khaki.vercel.app/",
+]
 
 # CORS configuration, needed for frontend development
 app.add_middleware(
@@ -32,28 +29,7 @@ app.add_middleware(
 )
 
 
-# global http exception handler, to handle errors
-@app.exception_handler(StarletteHTTPException)
-async def http_exception_handler(request, exc):
-    print(f"{repr(exc)}")
-    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
 
-# to use the settings
-@lru_cache()
-def get_settings():
-    return config.Settings()
-
-
-@app.get("/")
-def read_root(settings: config.Settings = Depends(get_settings)):
-    # print the app_name configuration
-    print(settings.app_name)
-    return "Hello World"
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
 
 
 
