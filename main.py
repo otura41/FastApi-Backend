adb87ca79311ff4082312256f6b5c9a0f4256c34
diosvanytorres3@gmail.com <130931255+otura41@users.noreply.github.com>
2025-06-08 22:44:25 -0400

    - Quita / de https://todo-frontend-khaki.vercel.app/

--- main.py
+++ main.py
@@ -29,6 +29,30 @@ app.add_middleware(
 )
 
 
+# global http exception handler, to handle errors
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    print(f"{repr(exc)}")
+    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+
+# to use the settings
+@lru_cache()
+def get_settings():
+    return config.Settings()
+
+
+@app.get("/")
+def read_root(settings: config.Settings = Depends(get_settings)):
+    # print the app_name configuration
+    print(settings.app_name)
+    return "Hello World"
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
 
 
 
@@ -59,14 +83,13 @@ app.add_middleware(
 #     print("Entrando a read_root")  # Log en consola
 #     return {"message": "Hello World"}
 
-@app.get("/todos")
-def get_todos():
-     return db.query(Todo).all()
+# @app.get("/todos")
+# def get_todos():
+#     return db.query(Todo).all()
 
-@app.get("/todos")
-def get_todos():
-     return [
-         {"id": 1, "name": "Tarea de ejemplo", "completed": False},
-         {"id": 2, "name": "Otra tarea", "completed": True}
-     ]
-
+# @app.get("/todos")
+# def get_todos():
+#     return [
+#         {"id": 1, "name": "Tarea de ejemplo", "completed": False},
+#         {"id": 2, "name": "Otra tarea", "completed": True}
+#     ]
