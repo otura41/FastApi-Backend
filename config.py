1b1f561f47f03ea2021150899b3cd33ad6da7adc
diosvanytorres3@gmail.com <130931255+otura41@users.noreply.github.com>
2025-06-08 22:13:23 -0400

    - Simplifica la class

--- config.py
+++ config.py
@@ -14,31 +14,46 @@
 
 # settings = Settings()
 
+# from pydantic_settings import BaseSettings
+# from dotenv import load_dotenv
+
+# load_dotenv()  # Carga las variables del archivo .env
+
+# class Settings(BaseSettings):
+#     DATABASE_HOST: str
+#     DATABASE_NAME: str
+#     DATABASE_USER: str
+#     DATABASE_PASSWORD: str
+#     DATABASE_PORT: int
+#     APP_NAME: str = "Full Stack To Do App"
+
+#     # class Config:
+#     #     env_file = ".env"
+#     #     extra = "ignore"
+
+#     # model_config = {
+#     #     "env_file": ".env",
+#     #     "extra": "ignore"
+#     # }
+
+#     class Config:
+#         env_file = ".env"
+#         extra = "ignore"
+
+
+# settings = Settings()
+
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 
 load_dotenv()  # Carga las variables del archivo .env
 
 class Settings(BaseSettings):
-    DATABASE_HOST: str
-    DATABASE_NAME: str
-    DATABASE_USER: str
-    DATABASE_PASSWORD: str
-    DATABASE_PORT: int
+    DATABASE_URL: str
     APP_NAME: str = "Full Stack To Do App"
-
-    # class Config:
-    #     env_file = ".env"
-    #     extra = "ignore"
-
-    # model_config = {
-    #     "env_file": ".env",
-    #     "extra": "ignore"
-    # }
 
     class Config:
         env_file = ".env"
         extra = "ignore"
-
 
 settings = Settings()
