b8f0ab6c9955f6eaaa88553818291d5b94b06259
diosvanytorres3@gmail.com <130931255+otura41@users.noreply.github.com>
2025-06-08 22:59:57 -0400

    - Quita / de https://todo-frontend-khaki.vercel.app/

--- main.py
+++ main.py
@@ -29,30 +29,9 @@ app.add_middleware(
 )
 
 
-# global http exception handler, to handle errors
-@app.exception_handler(StarletteHTTPException)
-async def http_exception_handler(request, exc):
-    print(f"{repr(exc)}")
-    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
-
-# to use the settings
-@lru_cache()
-def get_settings():
-    return config.Settings()
-
-
 @app.get("/")
-def read_root(settings: config.Settings = Depends(get_settings)):
-    # print the app_name configuration
-    print(settings.app_name)
-    return "Hello World"
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
+def read_root():
+    return {"message": "API funcionando correctamente"}
 
 
 
