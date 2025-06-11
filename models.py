b70ef3e31b91852b3da00f159e117545193a5c72
diosvanytorres3@gmail.com <130931255+otura41@users.noreply.github.com>
2025-06-10 21:14:07 -0400

    Actualizando

--- models.py
+++ models.py
@@ -4,9 +4,16 @@ from sqlalchemy.orm import relationship
 from database import Base
 
 
+# class ToDo(Base):
+#     __tablename__ = "todos"
+
+#     id = Column(Integer, primary_key=True, index=True)
+#     name = Column(String)
+#     completed = Column(Boolean, default=False)
+
 class ToDo(Base):
     __tablename__ = "todos"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
+    name = Column(String, nullable=False)
     completed = Column(Boolean, default=False)
